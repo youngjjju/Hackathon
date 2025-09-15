b4ef0514f5d7b6f65a6b4d17bcd9040e180288f1
Youngju Kim <2000dudwn@naver.com>
2025-09-15 18:40:16 +0900

    feat: main 초기화

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-# from module import image_loader
-
-# def main():
-#     image_path = 0  # Use 0 for webcam, or provide a video file path
-#     loader = image_loader.ImageLoader(image_path, imshow=True)
-    
-#     for frame in loader.frame_generator():
-#         # Process the frame (for demonstration, we just print its shape)
-#         print(f"Frame shape: {frame.shape}")
-        
-
 # if __name__ == "__main__":
 #     main()
