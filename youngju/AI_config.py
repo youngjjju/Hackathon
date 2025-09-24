8679cda38159211099ceaabb94fc87cc71da8a09
Youngju Kim <2000dudwn@naver.com>
2025-09-24 20:29:43 +0900

    feat: 3차 웨이트 추가

--- youngju/AI_config.py
+++ youngju/AI_config.py
@@ -2,7 +2,7 @@ from types import SimpleNamespace
 
 AI_config = SimpleNamespace(
     
-    weight_path = "/home/youngju/Hackathon/youngju/weight/video_classifier_fp16.onnx",
+    weight_path = "/home/youngju/Hackathon/youngju/weight/video_classifier.pth",
     image_path = 2,
     save_accident_path = "/home/youngju/accident_eval",
     save_non_accident_path = "/home/youngju/non_accident_eval",
